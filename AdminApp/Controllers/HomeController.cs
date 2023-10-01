62929a8a03c283955e9c2c34ba3e1783037ab4b9
michaelwilkins <micha@LAPTOP-24N15AAC>
2023-10-01 15:09:23 +0100

    Added admin application

---
+++ AdminApp/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using AdminApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using MOTStatusWebApi.Interfaces;
+using System.Diagnostics;
+using System.Linq;
+
+namespace AdminApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IMOTStatusDetailsRepository _statusDetailsRepository;
+
+        public HomeController(ILogger<HomeController> logger, IMOTStatusDetailsRepository statusDetailsRepository)
+        {
+            _logger = logger;
+            _statusDetailsRepository = statusDetailsRepository;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(string registration)
+        {
+            var details = _statusDetailsRepository.GetStatusDetails().Where(d => d.RegistrationNumber == registration).FirstOrDefault();
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
