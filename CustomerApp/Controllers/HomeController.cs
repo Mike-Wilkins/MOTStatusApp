dfa1cdcfd4dd2238ee3db44baeace08d38d1d9d0
michaelwilkins <micha@LAPTOP-24N15AAC>
2023-10-12 14:51:35 +0100

    Consume API and add detail confirmation page

--- CustomerApp/Controllers/HomeController.cs
+++ CustomerApp/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using MOTStatusWebApi.Data;
 using Newtonsoft.Json;
 using System.Diagnostics;
+using System.Text.RegularExpressions;
 
 namespace CustomerApp.Controllers
 {
@@ -28,14 +29,48 @@ namespace CustomerApp.Controllers
         [HttpPost]
         public async Task<IActionResult> Index(string registration)
         {
-            var carDetail = JsonConvert.DeserializeObject<MOTStatusDetails>(await client.GetStringAsync(url + registration));
+            if (registration == null)
+            {
+                ViewBag.RegistrationValidationError = true;
+                ViewBag.RegistrationFormatError = false;
+                return View();
+            }
 
-            return View(carDetail);
+            var regexValidation = VehicleRegEx(registration);
+
+            if (!regexValidation)
+            {
+                ViewBag.RegistrationValidationError = false;
+                ViewBag.RegistrationFormatError = true;
+                return View();
+            }
+
+            try
+            {
+                var carDetail = JsonConvert.DeserializeObject<MOTStatusDetails>(await client.GetStringAsync(url + registration));             
+                return RedirectToAction("DetailConfirmation", carDetail );
+            }
+            catch (Exception ex)
+            {
+                ViewBag.RegistrationNotFoundError = true;
+                return View();
+            }        
+        }
+
+        public IActionResult DetailConfirmation(MOTStatusDetails carDetails)
+        {
+            return View(carDetails);
         }
 
 
 
+        private bool VehicleRegEx(string registration)
+        {
+            var regexPattern = @"/^([A-Z]{3}\s?(\d{3}|\d{2}|d{1})\s?[A-Z])|([A-Z]\s?(\d{3}|\d{2}|\d{1})\s?[A-Z]{3})|(([A-HK-PRSVWY][A-HJ-PR-Y])\s?([0][2-9]|[1-9][0-9])\s?[A-HJ-PR-Z]{3})$/";
+            var regExIsValid = Regex.IsMatch(registration, regexPattern);
 
+            return regExIsValid;
+        }
 
 
         public IActionResult Privacy()
