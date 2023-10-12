d38ef1bd80a4aa94d726af8cc59d605be9c6523d
michaelwilkins <micha@LAPTOP-24N15AAC>
2023-10-12 14:02:34 +0100

    Created Customer App

---
+++ CustomerApp/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using CustomerApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using MOTStatusWebApi.Data;
+using Newtonsoft.Json;
+using System.Diagnostics;
+
+namespace CustomerApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        HttpClient client = new HttpClient();
+        string url = "https://localhost:7132/api/MOTStatusDetails/11/registration?registration=";
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            ViewBag.RegistrationValidationError = false;
+            ViewBag.RegistrationFormatError = false;
+            ViewBag.RegistrationNotFoundError = false;
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(string registration)
+        {
+            var carDetail = JsonConvert.DeserializeObject<MOTStatusDetails>(await client.GetStringAsync(url + registration));
+
+            return View(carDetail);
+        }
+
+
+
+
+
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
